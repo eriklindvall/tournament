07fc63aa5dccd419b591fa4ab9de9ae824a597b4
eriklindvall <erik.lindvall@gmail.com>
2017-10-12 22:12:52 +0200

    Match and TeamController

---
+++ Tournament/Controllers/MatchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Tournament.Models;
+
+namespace Tournament.Controllers
+{
+    public class MatchController : ApiController
+    {
+        private TournamentDbContext _dbContext = new TournamentDbContext();
+        
+        public IEnumerable<Match> Get()
+        {
+            return _dbContext.Matches;
+        }
+
+        public Match Get(int id)
+        {
+            return _dbContext
+                .Matches
+                .Include("HomeTeam")
+                .Include("AwayTeam")
+                .SingleOrDefault(m => m.Id == id);
+        }
+
+        public Match Post([FromBody]Match match)
+        {
+            _dbContext.Matches.Add(match);
+            _dbContext.SaveChanges();
+            return match;
+        }
+    }
+}
---
+++ Tournament/Controllers/TeamController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Tournament.Models;
+
+namespace Tournament.Controllers
+{
+    public class TeamController : ApiController
+    {
+        private TournamentDbContext _dbContext = new TournamentDbContext();
+        
+        public IEnumerable<Team> Get()
+        {
+            return _dbContext.Teams;
+        }
+
+        public Team Get(int id)
+        {
+            return _dbContext.Teams.Find(id);
+        }
+
+        public Team Post([FromBody]Team team)
+        {
+            _dbContext.Teams.Add(team);
+            _dbContext.SaveChanges();
+            return team;
+        }
+    }
+}
