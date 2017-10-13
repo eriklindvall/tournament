0a42410b0369e8712753653ce84aeb457ec9df08
eriklindvall <erik.lindvall@gmail.com>
2017-10-13 18:31:16 +0200

    Revert accidental remove of MatchResult

---
+++ Tournament/Models/ViewModels/MatchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tournament.Models.ViewModels
+{
+    public class MatchResult
+    {
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public int? HomeOvertimeScore { get; set; }
+        public int? AwayOvertimeScore { get; set; }
+        public int? HomePenaltyScore { get; set; }
+        public int? AwayPenaltyScore { get; set; }
+    }
+}
